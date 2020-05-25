c638b2c6d1bcc61031af719a2a3d4746442eb638
AdminingSS <admining@mail.ru>
2020-05-25 18:21:07 +0300

    html - scroll js

--- src/scripts/vendors/customscrollbar.js
+++ src/scripts/vendors/customscrollbar.js
@@ -8,14 +8,11 @@ $(window).on("load", function() {
     (() => {
         const $customScrollbars = $('.js-custom-scrollbar');
 
-        ($('<div><div /><div /></div>')).appendTo($('body'));
-
         $customScrollbars.mCustomScrollbar({
             theme: 'dark',
             setTop: 0,
             scrollInertia: 50,
             autoDraggerLength: false,
-            live: "on",
             mouseWheel:{ scrollAmount: 200 },
             advanced:{ updateOnContentResize: true }
         });
