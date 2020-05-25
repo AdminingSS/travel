71b0c6f57fd8c2aa1aa0e0b4b9c716b1b4041525
AdminingSS <admining@mail.ru>
2020-05-25 16:20:46 +0300

    html - jssss

--- src/scripts/vendors/customscrollbar.js
+++ src/scripts/vendors/customscrollbar.js
@@ -1,4 +1,5 @@
-import './customScrollbar/jquery.mCustomScrollbar.concat.min';
+//import './customScrollbar/jquery.mCustomScrollbar.concat.min';
+import './customScrollbar/jquery.mCustomScrollbar.concat.min.v3';
 import './customScrollbar/jquery.mCustomScrollbar.min.css'
 
 
@@ -7,12 +8,15 @@ $(window).on("load", function() {
     (() => {
         const $customScrollbars = $('.js-custom-scrollbar');
 
+        ($('<div><div /><div /></div>')).appendTo($('body'));
+
         $customScrollbars.mCustomScrollbar({
             theme: 'dark',
             setTop: 0,
             scrollInertia: 50,
             autoDraggerLength: false,
-            mouseWheel:{ scrollAmount: 300 },
+            live: "on",
+            mouseWheel:{ scrollAmount: 200 },
             advanced:{ updateOnContentResize: true }
         });
 
